317d5b35fdef7369cb6667fe64b252609cbbf939
guzlej <martin.guzlej@gmail.com>
2022-02-01 14:16:08 +0100

    fixed app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import unsplash from '../api/unsplash';
-import SearchBar from './SearchBar';
+import React from "react";
+import unsplash from "../api/unsplash";
+import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
-  state = { images: [] };
+  state = {
+    images: [],
+  };
 
   onSearchSubmit = async (term) => {
-    const response = await unsplash.get('/search/photos', {
-        params: { query: term },
-      });
-      this.setState({ images: response.data.results });
-  }
-  
+    const res = await unsplash.get("/search/photos", {
+      params: {
+        query: term,
+      },
+    });
+
+    this.setState({
+      images: res.data.results,
+    });
+  };
+
   render() {
-    return(
-      <div className="ui container" style={{ marginTop: '10px'}}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>    
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} image(s)
+        <ImageList images={this.state.images} />
       </div>
-    )
+    );
   }
 }
 
